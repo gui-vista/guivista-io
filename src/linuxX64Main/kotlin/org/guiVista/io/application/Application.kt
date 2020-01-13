18dceec198bfec034a3ef6cd9e3d2d1be70d11df
Nick Apperley <napperley@gmail.com>
2020-01-13 20:56:28 +1300

    Added Application, and ApplicationBase.

---
+++ src/linuxX64Main/kotlin/org/guiVista/io/application/Application.kt
@@ -0,0 +1,11 @@
+package org.guiVista.io.application
+
+import gio2.GApplication
+import gio2.G_APPLICATION_FLAGS_NONE
+import gio2.g_application_new
+import kotlinx.cinterop.CPointer
+
+/** Core application class. */
+class Application(id: String = "org.example.app") : ApplicationBase {
+    override val gApplicationPtr: CPointer<GApplication>? = g_application_new(id, G_APPLICATION_FLAGS_NONE)
+}
---
+++ src/linuxX64Main/kotlin/org/guiVista/io/application/application_base.kt
@@ -0,0 +1,108 @@
+package org.guiVista.io.application
+
+import gio2.*
+import glib2.TRUE
+import glib2.g_object_unref
+import glib2.gpointer
+import kotlinx.cinterop.CFunction
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.toKString
+import org.guiVista.core.ObjectBase
+import org.guiVista.core.connectGSignal
+import org.guiVista.core.disconnectGSignal
+
+private const val ACTIVATE_SIGNAL = "activate"
+private const val STARTUP_SIGNAL = "startup"
+private const val SHUTDOWN_SIGNAL = "shutdown"
+
+/** Base interface for application objects. */
+interface ApplicationBase : ObjectBase {
+    val gApplicationPtr: CPointer<GApplication>?
+    /** The unique identifier for the application. Default value is *""* (an empty String). */
+    val appId: String
+        get() = g_application_get_application_id(gApplicationPtr)?.toKString() ?: ""
+    /** Time (in ms) to stay alive after becoming idle. Default value is *0*. */
+    var inactivityTimeout: UInt
+        get() = g_application_get_inactivity_timeout(gApplicationPtr)
+        set(value) = g_application_set_inactivity_timeout(gApplicationPtr, value)
+    /** The base resource path for the application. Default value is *""* (an empty String). */
+    var resourceBasePath: String
+        get() = g_application_get_resource_base_path(gApplicationPtr)?.toKString() ?: ""
+        set(value) = g_application_set_resource_base_path(gApplicationPtr, value)
+    /**
+     * Whether the application is currently marked as busy through `g_application_mark_busy()`, or
+     * `g_application_bind_busy_property()`. Default value is *false*.
+     */
+    val isBusy: Boolean
+        get() = g_application_get_is_busy(gApplicationPtr) == TRUE
+    /** If `g_application_register()` has been called. Default value is *false*. */
+    val isRegistered: Boolean
+        get() = g_application_get_is_registered(gApplicationPtr) == TRUE
+    /** If this application instance is remote. Default value is *false*. */
+    val isRemote: Boolean
+        get() = g_application_get_is_remote(gApplicationPtr) == TRUE
+
+    /**
+     * Connects the *activate* signal to a [slot] on a application. This signal is used for initialising the
+     * application window, and is emitted on the primary instance when an activation occurs.
+     * @param slot The event handler for the signal.
+     * @param userData User data to pass through to the [slot].
+     */
+    fun connectActivateSignal(slot: CPointer<ActivateSlot>, userData: gpointer): ULong =
+        connectGSignal(obj = gApplicationPtr, signal = ACTIVATE_SIGNAL, slot = slot, data = userData)
+
+    /**
+     * Connects the *startup* signal to a [slot] on a application. This signal is emitted on the primary
+     * instance immediately after registration.
+     * @param slot The event handler for the signal.
+     * @param userData User data to pass through to the [slot].
+     */
+    fun connectStartupSignal(slot: CPointer<StartupSlot>, userData: gpointer): ULong =
+        connectGSignal(obj = gApplicationPtr, signal = STARTUP_SIGNAL, slot = slot, data = userData)
+
+    /**
+     * Connects the *shutdown* signal to a [slot] on a application. The *shutdown* signal is emitted only on the
+     * registered primary instance immediately after the main loop terminates.
+     * @param slot The event handler for the signal.
+     * @param userData User data to pass through to the [slot].
+     */
+    fun connectShutdownSignal(slot: CPointer<ShutdownSlot>, userData: gpointer): ULong =
+        connectGSignal(obj = gApplicationPtr, signal = SHUTDOWN_SIGNAL, slot = slot, data = userData)
+
+    /**
+     * Runs the application.
+     * @return A status code is returned when the application exits. Any code not equal to *0* means a error has
+     * occurred.
+     */
+    fun run(): Int = g_application_run(gApplicationPtr, 0, null)
+
+    override fun disconnectSignal(handlerId: ULong) {
+        super.disconnectSignal(handlerId)
+        disconnectGSignal(gApplicationPtr, handlerId)
+    }
+
+    override fun close() {
+        g_object_unref(gApplicationPtr)
+    }
+}
+
+/**
+ * The event handler for the *activate* signal. Arguments:
+ * 1. application: CPointer<GApplication>
+ * 2. userData: gpointer
+ */
+typealias ActivateSlot = CFunction<(application: CPointer<GApplication>, userData: gpointer) -> Unit>
+
+/**
+ * The event handler for the *startup* signal. Arguments:
+ * 1. application: CPointer<GApplication>
+ * 2. userData: gpointer
+ */
+typealias StartupSlot = CFunction<(application: CPointer<GApplication>, userData: gpointer) -> Unit>
+
+/**
+ * The event handler for the *shutdown* signal. Arguments:
+ * 1. application: CPointer<GApplication>
+ * 2. userData: gpointer
+ */
+typealias ShutdownSlot = CFunction<(application: CPointer<GApplication>, userData: gpointer) -> Unit>
