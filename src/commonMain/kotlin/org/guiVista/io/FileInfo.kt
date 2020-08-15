e7a9a10be563bbcac6121d4646670d1cd6f78fb8
Nick Apperley <napperley@gmail.com>
2020-08-16 11:38:48 +1200

    Included additional functions/properties in the FileInfo class.

--- src/commonMain/kotlin/org/guiVista/io/FileInfo.kt
+++ src/commonMain/kotlin/org/guiVista/io/FileInfo.kt
@@ -21,4 +21,194 @@ expect class FileInfo : Closable {
 
     /** Edit name for the file. */
     var editName: String
+
+    /** When *true* the file is a backup file. */
+    val isBackup: Boolean
+
+    /** The icon for the file. */
+    var icon: Icon
+
+    /** The symbolic icon for the file */
+    var symbolicIcon: Icon
+
+    /** File size in bytes. */
+    var size: Long
+
+    /** The symbolic link target. */
+    var symlinkTarget: String
+
+    /** The entity tag. Refer to `G_FILE_ATTRIBUTE_ETAG_VALUE`. Based on the `g_file_info_get_etag` function. */
+    val eTag: String
+
+    /** The value of the sort order attribute. Refer to `G_FILE_ATTRIBUTE_STANDARD_SORT_ORDER`. */
+    var sortOrder: Int
+
+    /**
+     * Checks if [FileInfo] has an attribute that matches [attr].
+     * @param attr File attribute key.
+     * @return A value of *true* if [FileInfo] has an attribute matching [attr], *false* otherwise.
+     */
+    fun hasAttribute(attr: String): Boolean
+
+    /**
+     * Checks if [FileInfo] has an attribute that matches [namespace].
+     * @param namespace File attribute namespace.
+     * @return A value of *true* if [FileInfo] has an attribute matching [namespace], *false* otherwise.
+     */
+    fun hasNamespace(namespace: String): Boolean
+
+    /**
+     * Lists the [FileInfo] attributes.
+     * @param namespace The file attribute key's namespace, or *""* to list **ALL** attributes.
+     * @return An array of strings of all of the possible attribute types for the [namespace], or an empty array on
+     * error.
+     */
+    fun listAttributes(namespace: String): Array<String>
+
+    /**
+     * Removes all cases of [attr] from [FileInfo] if it exists.
+     * @param attr A file attribute key.
+     */
+    fun removeAttribute(attr: String)
+
+    /**
+     * Gets the value of [attr] formatted as a String. This escapes things as needed to make the string valid UTF-8.
+     * Based on the `g_file_info_get_attribute_as_string` function.
+     * @param attr A file attribute key.
+     * @return A UTF-8 string associated with the given [attribute][attr], or *""* if the [attribute][attr] wasn’t set.
+     */
+    fun fetchAttributeAsString(attr: String): String
+
+    /**
+     * Gets the value of a String attribute. Based on the `g_file_info_get_attribute_string` function.
+     * @param attr A file attribute key.
+     * @return The contents of the attribute value as a UTF-8 String, or *""* otherwise.
+     */
+    fun fetchStringAttribute(attr: String): String
+
+    /**
+     * Gets the value of a StringV attribute. Based on the `g_file_info_get_attribute_stringv` function.
+     * @param attr A file attribute key.
+     * @return the contents of the attribute value as a StringV (an array), or a empty array. All returned strings are
+     * UTF-8.
+     */
+    fun fetchStringVAttribute(attr: String): Array<String>
+
+    /**
+     * Gets the value of a Byte String attribute. Based on the `g_file_info_get_attribute_byte_string` function.
+     * @param attr A file attribute key.
+     * @return The contents of the attribute value as a Byte String, or *""* otherwise.
+     */
+    fun fetchByteStringAttribute(attr: String): String
+
+    /**
+     * Gets the value of a Boolean attribute. Based on the `g_file_info_get_attribute_boolean` function.
+     * @param attr A file attribute key.
+     * @return The Boolean value contained within the attribute. If [attr] doesn't have a value then *false* is
+     * returned.
+     */
+    fun fetchBooleanAttribute(attr: String): Boolean
+
+    /**
+     * Gets an unsigned 32-bit Integer contained within the [attribute][attr]. Based on the
+     * `g_file_info_get_attribute_uint32` function.
+     * @param attr A file attribute key.
+     * @return An unsigned 32-bit Integer from the attribute. If the attribute does not contain an unsigned 32-bit
+     * Integer, or is invalid then *0* will be returned.
+     */
+    fun fetchUInt32Attribute(attr: String): UInt
+
+    /**
+     * Gets a signed 32-bit Integer contained within the attribute. Based on the `g_file_info_get_attribute_int32`
+     * function.
+     * @param attr A file attribute key.
+     * @return A signed 32-bit Integer from the attribute. If the attribute doesn't contain a signed 32-bit Integer, or
+     * is invalid then *0* will be returned.
+     */
+    fun fetchInt32Attribute(attr: String): Int
+
+    /**
+     * Gets an unsigned 64-bit Integer contained within the [attribute][attr]. Based on the
+     * `g_file_info_get_attribute_uint64` function.
+     * @param attr A file attribute key.
+     * @return An unsigned 64-bit Integer from the attribute. If the attribute does not contain an unsigned 64-bit
+     * Integer, or is invalid then *0* will be returned.
+     */
+    fun fetchUInt64Attribute(attr: String): ULong
+
+    /**
+     * Gets a signed 64-bit Integer contained within the attribute. Based on the `g_file_info_get_attribute_int64`
+     * function.
+     * @param attr A file attribute key.
+     * @return A signed 64-bit Integer from the attribute. If the attribute doesn't contain a signed 64-bit Integer, or
+     * is invalid then *0* will be returned.
+     */
+    fun fetchInt64Attribute(attr: String): Long
+
+    /**
+     * Sets the String [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_string` function.
+     * @param attr A file attribute key.
+     * @param value A UTF-8 String.
+     */
+    fun changeStringAttribute(attr: String, value: String)
+
+    /**
+     * Sets the String V [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_stringv` function.
+     * @param attr A file attribute key.
+     * @param value An array of UTF-8 strings.
+     */
+    fun changeStringVAttribute(attr: String, value: Array<String>)
+
+    /**
+     * Sets the Byte String [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_byte_string` function.
+     * @param attr A file attribute key.
+     * @param value A Byte String.
+     */
+    fun changeByteStringAttribute(attr: String, value: String)
+
+    /**
+     * Sets the Boolean [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_boolean` function.
+     * @param attr A file attribute key.
+     * @param value A Boolean value.
+     */
+    fun changeBooleanAttribute(attr: String, value: Boolean)
+
+    /**
+     * Sets the UInt32 [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_uint32` function.
+     * @param attr A file attribute key.
+     * @param value An unsigned 32-bit integer.
+     */
+    fun changeUInt32Attribute(attr: String, value: UInt)
+
+    /**
+     * Sets the Int32 [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_int32` function.
+     * @param attr A file attribute key.
+     * @param value A signed 32-bit integer.
+     */
+    fun changeInt32Attribute(attr: String, value: Int)
+
+    /**
+     * Sets the UInt64 [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_uint64` function.
+     * @param attr A file attribute key.
+     * @param value An unsigned 64-bit integer.
+     */
+    fun changeUInt64Attribute(attr: String, value: ULong)
+
+    /**
+     * Sets the UInt64 [attribute][attr] to contain the given [value], if possible. Based on the
+     * `g_file_info_set_attribute_int64` function.
+     * @param attr A file attribute key.
+     * @param value An unsigned 64-bit integer.
+     */
+    fun changeInt64Attribute(attr: String, value: Long)
+
+    /** Clears the status information from [FileInfo]. */
+    fun clearStatus()
 }
--- src/linuxArm32Main/kotlin/org.guiVista.io/FileInfo.kt
+++ src/linuxArm32Main/kotlin/org.guiVista.io/FileInfo.kt
@@ -3,9 +3,9 @@ package org.guiVista.io
 import gio2.*
 import glib2.FALSE
 import glib2.TRUE
+import glib2.g_free
 import glib2.g_object_unref
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.toKString
+import kotlinx.cinterop.*
 import org.guiVista.core.Closable
 
 actual class FileInfo(fileInfoPtr: CPointer<GFileInfo>? = null) : Closable {
@@ -13,7 +13,6 @@ actual class FileInfo(fileInfoPtr: CPointer<GFileInfo>? = null) : Closable {
     actual var isHidden: Boolean
         get() = g_file_info_get_is_hidden(gFileInfoPtr) == TRUE
         set(value) = g_file_info_set_is_hidden(gFileInfoPtr, if (value) TRUE else FALSE)
-
     actual var isSymlink: Boolean
         get() = g_file_info_get_is_symlink(gFileInfoPtr) == TRUE
         set(value) = g_file_info_set_is_symlink(gFileInfoPtr, if (value) TRUE else FALSE)
@@ -29,13 +28,120 @@ actual class FileInfo(fileInfoPtr: CPointer<GFileInfo>? = null) : Closable {
     actual var contentType: String
         get() = g_file_info_get_content_type(gFileInfoPtr)?.toKString() ?: ""
         set(value) = g_file_info_set_content_type(gFileInfoPtr, value)
-
-    /** The file size (in bytes). */
-    var size: goffset
+    actual var size: Long
         set(value) = g_file_info_set_size(gFileInfoPtr, value)
         get() = g_file_info_get_size(gFileInfoPtr)
+    actual val isBackup: Boolean
+        get() = g_file_info_get_is_backup(gFileInfoPtr) == TRUE
+    actual var icon: Icon
+        get() = Icon.fromIconPtr(g_file_info_get_icon(gFileInfoPtr))
+        set(value) = g_file_info_set_icon(gFileInfoPtr, value.gIconPtr)
+    actual var symbolicIcon: Icon
+        get() = Icon.fromIconPtr(g_file_info_get_icon(gFileInfoPtr))
+        set(value) = g_file_info_set_icon(gFileInfoPtr, value.gIconPtr)
+    actual var symlinkTarget: String
+        get() = g_file_info_get_symlink_target(gFileInfoPtr)?.toKString() ?: ""
+        set(value) = g_file_info_set_symlink_target(gFileInfoPtr, value)
+    actual val eTag: String
+        get() = g_file_info_get_etag(gFileInfoPtr)?.toKString() ?: ""
+    actual var sortOrder: Int
+        get() = g_file_info_get_sort_order(gFileInfoPtr)
+        set(value) = g_file_info_set_sort_order(gFileInfoPtr, value)
 
     override fun close() {
         g_object_unref(gFileInfoPtr)
+    }
+
+    actual fun hasAttribute(attr: String): Boolean = g_file_info_has_attribute(gFileInfoPtr, attr) == TRUE
+
+    actual fun hasNamespace(namespace: String): Boolean = g_file_info_has_namespace(gFileInfoPtr, namespace) == TRUE
+
+    actual fun listAttributes(namespace: String): Array<String> {
+        val tmpList = mutableListOf<String>()
+        var pos = 0
+        var item: CPointer<ByteVarOf<Byte>>?
+        val attrTmp = g_file_info_list_attributes(gFileInfoPtr, namespace)
+        do {
+            item = attrTmp?.get(pos)
+            if (item != null) tmpList += item.toKString()
+            pos++
+        } while (item != null)
+        return tmpList.toTypedArray()
+    }
+
+    actual fun removeAttribute(attr: String) {
+        g_file_info_remove_attribute(gFileInfoPtr, attr)
+    }
+
+    actual fun fetchAttributeAsString(attr: String): String {
+        val tmp = g_file_info_get_attribute_as_string(gFileInfoPtr, attr)
+        val result = tmp?.toKString() ?: ""
+        g_free(tmp)
+        return result
+    }
+
+    actual fun fetchStringAttribute(attr: String): String =
+        g_file_info_get_attribute_string(gFileInfoPtr, attr)?.toKString() ?: ""
+
+    actual fun fetchStringVAttribute(attr: String): Array<String> {
+        val tmpList = mutableListOf<String>()
+        var pos = 0
+        var item: CPointer<ByteVarOf<Byte>>?
+        val tmpValue = g_file_info_get_attribute_stringv(gFileInfoPtr, attr)
+        do {
+            item = tmpValue?.get(pos)
+            if (item != null) tmpList += item.toKString()
+            pos++
+        } while (item != null)
+        return tmpList.toTypedArray()
+    }
+
+    actual fun fetchByteStringAttribute(attr: String): String =
+        g_file_info_get_attribute_byte_string(gFileInfoPtr, attr)?.toKString() ?: ""
+
+    actual fun fetchBooleanAttribute(attr: String): Boolean =
+        g_file_info_get_attribute_boolean(gFileInfoPtr, attr) == TRUE
+
+    actual fun fetchUInt32Attribute(attr: String): UInt = g_file_info_get_attribute_uint32(gFileInfoPtr, attr)
+
+    actual fun fetchInt32Attribute(attr: String): Int = g_file_info_get_attribute_int32(gFileInfoPtr, attr)
+
+    actual fun fetchUInt64Attribute(attr: String): ULong = g_file_info_get_attribute_uint64(gFileInfoPtr, attr)
+
+    actual fun fetchInt64Attribute(attr: String): Long = g_file_info_get_attribute_int64(gFileInfoPtr, attr)
+
+    actual fun changeStringAttribute(attr: String, value: String) =
+        g_file_info_set_attribute_string(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun changeStringVAttribute(attr: String, value: Array<String>) = memScoped {
+        val tmp = value.toCStringArray(this)
+        g_file_info_set_attribute_stringv(info = gFileInfoPtr, attribute = attr, attr_value = tmp)
+        Unit
+    }
+
+    actual fun changeByteStringAttribute(attr: String, value: String) =
+        g_file_info_set_attribute_byte_string(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun changeBooleanAttribute(attr: String, value: Boolean) =
+        g_file_info_set_attribute_boolean(
+            info = gFileInfoPtr,
+            attribute = attr,
+            attr_value = if (value) TRUE else FALSE
+        )
+
+    actual fun changeUInt32Attribute(attr: String, value: UInt) =
+        g_file_info_set_attribute_uint32(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun changeInt32Attribute(attr: String, value: Int) =
+        g_file_info_set_attribute_int32(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun changeUInt64Attribute(attr: String, value: ULong) =
+        g_file_info_set_attribute_uint64(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun changeInt64Attribute(attr: String, value: Long) =
+        g_file_info_set_attribute_int64(info = gFileInfoPtr, attribute = attr, attr_value = value)
+
+    actual fun clearStatus() {
+        g_file_info_clear_status(gFileInfoPtr)
     }
 }
